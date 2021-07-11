5f8b809142cf6e48ac33d174b158dd9a73702adb
LuizSouz404 <luiz.souz404@gmail.com>
2021-07-11 20:11:02 -0300

    Start: Create Backend Node.js

---
+++ BackEnd/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+
+const server = express();
+
+server.use(express.static("public"));
+
+server.set('view engine', 'ejs');
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(routes);
+
+server.listen(3333, () => {
+  console.log("Server is running on Port 3333!")
+})
