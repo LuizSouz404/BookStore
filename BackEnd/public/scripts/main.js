68675c94ae0d2914afc33e8778a400b43fd268ba
LuizSouz404 <luiz.souz404@gmail.com>
2021-07-15 20:48:08 -0300

    Chore: Refact styles

--- BackEnd/public/scripts/main.js
+++ BackEnd/public/scripts/main.js
@@ -1,8 +1,17 @@
 import Modal from './modal.js';
 
 const modal = Modal();
-
 const requestButton = document.querySelector("header a.request");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.querySelector("header").classList.add("active");
+  } else {
+    document.querySelector("header").classList.remove("active");
+  }
+}
 
 requestButton.addEventListener("click", event => {
   event.preventDefault();
